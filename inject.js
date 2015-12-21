4d8bce7ece6d7174f4d6862f24f9072e55a62dc9
mchaconcr <marco@eat.tv>
2015-12-21 12:14:46 -0600

    test

---
+++ inject.js
@@ -0,0 +1,4 @@
+jQuery('#navStyle').remove();
+jQuery("<link/>", {id: "navStyle", rel: "stylesheet",type: "text/css",href: "http://www.eat.tv/CNNNav/breakpoints/fixes.css?cache="+Math.random()}).appendTo("head");
+
+
